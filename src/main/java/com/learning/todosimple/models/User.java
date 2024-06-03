617fcfee848fdcdc1255ce705b6da24f39448485
GleidesonFr <gleidesonfreitas015@gmail.com>
2024-06-03 06:26:37 -0300

    model User has been created

---
+++ src/main/java/com/learning/todosimple/models/User.java
@@ -0,0 +1,69 @@
+package com.learning.todosimple.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table (name = User.TABLE_NAME)
+public class User {
+    public interface CreateUser{}
+    public interface UpdateUser{}
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotBlank(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
+    private String username;
+
+    @Column(name = "password", length = 60, nullable = false)
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    private String password;
+
+    //private List<Task> tasks = new ArrayList<TasK>()
+
+    public User(Long id, String username, String password){
+        this.id = id;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+}
