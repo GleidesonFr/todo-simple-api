c2981da92425762b14e938b185523f4c99614fb7
GleidesonFr <gleidesonfreitas015@gmail.com>
2024-06-04 12:35:12 -0300

    Task.java has been created

---
+++ src/main/java/com/learning/todosimple/models/Task.java
@@ -0,0 +1,85 @@
+package com.learning.todosimple.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table (name = Task.TABLE_NAME)
+public class Task {
+
+    public static final String TABLE_NAME = "task";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)  //referenciar coluna de outra tabela
+    private User user;
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String description;
+
+    public Task(){}
+
+    public Task(Long id, User user, String description){
+        this.id = id;
+        this.user = user;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this){
+            return true;
+        }else if(!(obj instanceof User)){
+            return false;
+        }
+
+        Task task = (Task) obj;
+        return Objects.equals(this.id, task.id) && Objects.equals(this.user, task.user) && Objects.equals(this.description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return Objects.hash(this.id, this.user, this.description);
+    }
+}
